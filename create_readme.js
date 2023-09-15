23452edf68d70dc66f6bcb918cc116d54cbb72f2
Eric Ryan Harrison <me@ericharrison.info>
2023-09-15 15:09:06 -0400

    Better tooling!

--- create_readme.js
+++ create_readme.js
@@ -1,26 +1,36 @@
+const problems = require('./problems');
+
 // simple function to create the README files and keep
 // index.js clean.
 function create_readme(readme_type, readme_data) {
 	if ( readme_type === 'user' ) {
+		const problemname = problems[readme_data.problem];
 		const username = readme_data.username;
 		const problem = readme_data.problem;
 		const language = readme_data.language;
 		const markdown = readme_data.markdown;
 		return `
-# ${username}'s Euler Solution ${problem} [${language}
-This is the solution for @${username}. 
+# ${username} - ${problem}. ${problemname}
+## Euler Solution in ${language}
+https://projecteuler.net/problem=${problem} 
+
+This is the Project Euler solution for [${problem}. ${problemname}](https://projecteuler.net/problem=${problem})
+written by ${username} in ${language}.
 
 ## Problem Description
 ${markdown}
 `;
 	} else if ( readme_type === 'problem' ) {
+		const problemname = problems[readme_data.problem];
 		const problem   = readme_data.problem;
 		const markdown  = readme_data.markdown;
 		const languages = readme_data.languages;
 		
 		let output = `
-# Euler e${problem} Solutions
+# Solutions for ${problem}. - ${problemname}
 ## Problem Description
+https://projecteuler.net/problem=${problem}
+
 ${markdown}
 
 ## Languages
@@ -38,13 +48,14 @@ ${markdown}
 		return output;
 
 	} else if ( readme_type === 'language' ) {
+		const problemname    = problems[readme_data.problem];
 		const problem        = readme_data.problem;
 		const markdown       = readme_data.markdown;
 		const language       = readme_data.language;
 		const language_users = readme_data.language_users;
 		
 		let output = `
-# Euler e${problem} Solutions for ${language}
+# ${language} Solutions for ${problem}. ${problemname}
 ## Problem Description
 ${markdown}
 
@@ -56,8 +67,48 @@ ${markdown}
 		}
 	
 		return output;
+	} else if ( readme_type === 'leaderboard' ) {
+		const submissions = readme_data.submissions;
+
+		let output = `# theClubhou.se Project Euler Collaboration Repository
+## Submissions Leaderboard
+`;
+		const clean = process_submissions(submissions);
+
+		for ( const user in clean.leaderboard ) {
+			output += `1. @${user} **${clean.leaderboard[user]}** entries\n`;
+		}
+
+		output += '\n# Popular Languages\n';
+
+		const languages = Object.keys(clean.languages)
+			.sort((a,b) => clean.languages[b].count - clean.languages[a].count);
+		for ( let i = 0; i < languages.length; ++i ) {
+			output += `1. **${languages[i]}** with **${clean.languages[languages[i]].count}** entries [leader: @${clean.languages[languages[i]].max_user} - ${clean.languages[languages[i]].max}]\n`;
+		}
+		
+		return output;
+	}
+}
+
+function process_submissions(submissions) {
+	const processed = {
+		leaderboard: {},
+		languages: submissions.languages,
+		problems: submissions.problems,
+	};
+	const sorted = Object.keys(submissions.users)
+		.sort((a,b) => {
+			return submissions.users[b].count - submissions.users[a].count;
+		});
+	for ( let i = 0; i < sorted.length; ++i ) {
+		processed.leaderboard[sorted[i]] = submissions.users[sorted[i]].count;
+		if ( i >= 9 ) {
+			break;
+		}
 	}
 
+	return processed;
 }
 		
 module.exports = create_readme;
---
+++ sync_problems.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+const fs    = require('fs');
+
+console.log("Refreshing problems from https://projecteuler.net/minimal=problems");
+const problems = {};
+fetch(`https://projecteuler.net/minimal=problems`)
+	.then(data => data.text())
+	.then(list => {
+		const lines = list.split('\n');
+		// skip header row
+		for ( let i = 1; i < lines.length; ++i ) {
+			if ( lines[i].includes('#') ) {
+				const sections = lines[i].split('##');
+				problems[sections[0]] = sections[1];
+			}
+		}
+
+		fs.writeFileSync(`problems.js`, `module.exports = ${JSON.stringify(problems,null,'\t')};`);
+		console.log(`Wrote ${Object.keys(problems).pop()} problems to problems.js`);
+	});
