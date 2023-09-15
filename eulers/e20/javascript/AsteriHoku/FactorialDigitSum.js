b9ee56079febf9c1e004354fd7f479949298ef97
Sarah <sarah.matta@enformion.com>
2023-09-15 13:28:48 -0400

    e20 factorial digit sum

---
+++ eulers/e20/javascript/AsteriHoku/FactorialDigitSum.js
@@ -0,0 +1,14 @@
+const bigNum = 100n;//TODO define your number here 
+
+function getFactorial(n) {
+	if (n === 0n || n === 1n) return 1;
+	return n * BigInt(getFactorial(n - 1n));//recursion
+}
+
+function sumOfDigits(longNum) {
+	return longNum.toString().split('').map(Number)
+	  .reduce((sum, digit) => sum + digit, 0);
+}
+
+let ans = sumOfDigits(getFactorial(bigNum));
+console.log(`Sum of digits for ${bigNum}! gives answer ${ans}`);
