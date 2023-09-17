04d275a8c644142ba5980dc2f120641f68ffaec4
AsteriHoku <95447747+AsteriHoku@users.noreply.github.com>
2023-09-16 20:41:11 -0400

    Update eulers/e20/javascript/AsteriHoku/FactorialDigitSum.js
    Co-authored-by: Eric Ryan Harrison <me@ericharrison.info>

--- eulers/e20/javascript/AsteriHoku/FactorialDigitSum.js
+++ eulers/e20/javascript/AsteriHoku/FactorialDigitSum.js
@@ -1,6 +1,7 @@
 const bigNum = 100n;//TODO define your number here 
 
 function getFactorial(n) {
+	n = BigInt(n);
 	if (n === 0n || n === 1n) return 1;
 	return n * BigInt(getFactorial(n - 1n));//recursion
 }
